c4ac806df1efff479dc5f0169e623af22101d482
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-06-01 14:20:04 +0300

    add refs

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+};
 
+refs.startBtn.addEventListener('click', onBtnStart);
+
+
+function onBtnStart() {
+    
+}
