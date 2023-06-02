93222f526ffb7f74d80882aee350ddc941878c6c
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-06-02 21:31:03 +0300

    timer done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,8 +4,13 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
-
-}
+    startBtn: document.querySelector('[data-start]'),
+    dateInp: document.querySelector('#datetime-picker'),
+    daysVal: document.querySelector('[data-days]'),
+    hoursVal: document.querySelector('data-hours'),
+    minsVal: document.querySelector('[data-minutes]'),
+    secVal: document.querySelector('[data-seconds]'),
+};
 
 const options = {
   enableTime: true,
@@ -18,21 +23,28 @@ const options = {
 };
 
 const timer = {
+    intervalId: null,
+    isActive: false,
     start() {
-        const startTime = Date.now(); 
+        if (this.isActive) { // коли запускаємо таймер і він вже активний, 
+            return; // просто виходимо з коду
+        }
+        const startTime = Date.now(); // наш стартовий час
+        this.isActive = true; // робимо його яктивним (якщо він був не активний) і запускаємо інтервал
         
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = currentTime - startTime;
-            const { days, hours, minutes, seconds } = convertMs(deltaTime);
-            // console.log(currentTime);
-            // console.log(currentTime - startTime);
-            console.log({ days, hours, minutes, seconds });
-        }, 1000)
+        this.intervalId = setInterval(() => { // запускаємо інтервал, і при кожному визову фун-ії відкладенної 
+            const currentTime = Date.now(); // отримуємо поточний час
+            const deltaTime = currentTime - startTime; // різницю між поточним і стартовим (кількість мс)
+            const { days, hours, minutes, seconds } = convertMs(deltaTime); // визиваємо convertMS(рахує скільки влазить мілісекунд в дні, години, часи ...) і повертає в {days ...}
+            
+            console.log(`${days}:${hours}:${minutes}:${seconds}`);
+        }, 1000);
     },
-}
-timer.start();
+};
 
+refs.startBtn.addEventListener('click', () => {
+    timer.start();
+});
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -42,13 +54,26 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
-}
+};
+
+// приймає число, призводить до рядка і додає на початок 0 якщо число менше 2-х знаків
+function addLeadingZero(val) {
+    return String(val).padStart(2, '0');
+};
+
+// приймає час в (мс), рахує скільки поміщає в себе днів, годин, ..., виводить в інтерфейс
+function updateClockface({ daysVal, hoursVal, minsVal, secVal }) {
+    refs.daysVal.textContent = `${ daysVal }`;
+    refs.hoursVal.textContent = `${ hoursVal }`;
+    refs.minsVal.textContent = `${ minsVal }`;
+    refs.secVal.textContent = `${ secVal }`;
+};
