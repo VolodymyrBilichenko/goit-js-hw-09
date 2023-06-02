bde001131bed5dd77c9096e93bd9c8cfd2d00c68
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-06-02 12:40:25 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,32 @@
 const refs = {
     startBtn: document.querySelector('[data-start]'),
-    stopBtn: document.querySelector('[data-stop]')
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
 };
 
 refs.startBtn.addEventListener('click', onBtnStart);
-
+refs.stopBtn.addEventListener('click', onBtnStop);
+const INTERVAL_DELAY = 1000;
+let timerId = null;
 
 function onBtnStart() {
-    
+    if (timerId) {
+        return;
+    }
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, INTERVAL_DELAY);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function onBtnStop() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    timerId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
